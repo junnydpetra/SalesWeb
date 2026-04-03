4f90071202262177545f3c2754aa51a331288008
junnydpetra <junnycmenezes@gmail.com>
2026-04-03 20:35:53 -0300

    Firs Model-Controller-View - Department

---
+++ SalesWeb/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Models;
+
+namespace SalesWeb.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Electronics" });
+            list.Add(new Department { Id = 2, Name = "Books" });
+
+            return View(list);
+        }
+    }
+}
--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SalesWeb.Models;
 using System.Diagnostics;
+using SalesWeb.Models.ViewModels;
 
 namespace SalesWeb.Controllers
 {
